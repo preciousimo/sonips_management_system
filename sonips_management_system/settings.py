0bce50b6a095f4dc768feaafb3143157b992d204
preciousimo <preciousimoniakemu@gmail.com>
2022-10-30 22:19:58 +0100

    Included STATIC and TEMPLATE DIR

--- sonips_management_system/settings.py
+++ sonips_management_system/settings.py
@@ -122,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/img/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
