727632ec9a511649e14744252916fbb1e66fdaeb
preciousimo <preciousimoniakemu@gmail.com>
2022-10-31 11:10:57 +0100

    Base URL config

--- sonips_management_system/urls.py
+++ sonips_management_system/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sonips_admin.urls')), 
     path('account/', include('account.urls')), 
+    path('base/', include('base.urls')), 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
