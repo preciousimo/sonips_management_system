c8c05b3231e04ec9e8037a2551e892b3607e9132
preciousimo <preciousimoniakemu@gmail.com>
2023-02-10 00:12:58 +0100

    Fix HTTP_HOST bug

--- school_app/settings.py
+++ school_app/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOST = ['*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
