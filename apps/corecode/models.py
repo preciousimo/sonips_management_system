c2fbaf839eff61b1850b4f2be786aab0751f339d
preciousimo <preciousimoniakemu@gmail.com>
2023-02-09 22:30:16 +0100

    Remove site configuration

--- apps/corecode/models.py
+++ apps/corecode/models.py
@@ -3,16 +3,6 @@ from django.db import models
 # Create your models here.
 
 
-class SiteConfig(models.Model):
-    """Site Configurations"""
-
-    key = models.SlugField()
-    value = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.key
-
-
 class AcademicSession(models.Model):
     """Academic Session"""
 
