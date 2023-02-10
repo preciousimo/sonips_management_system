b9f4608660b43ca74f17bcb73ff6f652497729fe
preciousimo <preciousimoniakemu@gmail.com>
2023-02-10 13:09:34 +0100

    Register models in django admin

---
+++ apps/corecode/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import*
+
+admin.site.register(AcademicSession)
+admin.site.register(AcademicTerm)
+admin.site.register(Subject)
+admin.site.register(StudentClass)
--- apps/finance/admin.py
+++ apps/finance/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import*
 
-# Register your models here.
+admin.site.register(Invoice)
+admin.site.register(InvoiceItem)
+admin.site.register(Receipt)
