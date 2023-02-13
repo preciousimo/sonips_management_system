9e572dcbf4bd9cb265e54a1a06c4f40fa376720c
preciousimo <preciousimoniakemu@gmail.com>
2023-02-14 00:38:59 +0100

    Create utils for grad scores

--- apps/result/utils.py
+++ apps/result/utils.py
@@ -1,3 +1,16 @@
 def score_grade(score):
-    if score <= 10:
+    if score > 100:
+        return ValueError
+    elif score >= 70:
         return "A"
+    elif score >= 60 < 70:
+        return "B"
+    elif score >= 50 < 60:
+        return "C"
+    elif score >= 40 < 50:
+        return "D"
+    elif score >= 0 < 40:
+        return "F"
+    elif score < 0:
+        return ValueError
+
