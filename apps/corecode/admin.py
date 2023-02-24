6bd80e5406ca737718ddcc10090b5e35fefab749
preciousimo <preciousimoniakemu@gmail.com>
2023-02-24 21:19:41 +0100

    Create site configuration page

--- apps/corecode/admin.py
+++ apps/corecode/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import*
 
+admin.site.register(SiteConfig)
 admin.site.register(AcademicSession)
 admin.site.register(AcademicTerm)
 admin.site.register(Subject)
